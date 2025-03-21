c65dfdc7b8018227a5aabec35020d95de654d98a
Gnedor <ville7618@gmail.com>
2025-03-21 16:11:26 +0100

    script

--- Assets/Scripts/SpriteController.cs
+++ Assets/Scripts/SpriteController.cs
@@ -1,6 +1,26 @@
 using UnityEngine;
 
-public class NewEmptyCSharpScript
+public class SpriteController : MonoBehaviour
 {
-    
+    public Camera _mainCamera;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void LateUpdate()
+    {
+        Vector3 cameraPosition = _mainCamera.transform.position;
+        cameraPosition.y = transform.position.y;
+
+        transform.LookAt(cameraPosition);
+        transform.Rotate(0f, 180f, 0f);
+    } 
 }
