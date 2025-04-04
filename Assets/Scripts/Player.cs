d29cd3dd096c723a96c959cd2010a69062cd8f92
Gnedor <ville7618@gmail.com>
2025-04-04 16:21:42 +0200

    aoiwda

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,6 +2,8 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    public static int ammo = 5;
+    public int health;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
