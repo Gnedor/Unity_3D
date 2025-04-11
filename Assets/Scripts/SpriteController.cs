8dc8f57d4486e5acd79dd792407b91d7739ee55e
Gnedor <ville7618@gmail.com>
2025-04-11 15:19:55 +0200

    Revert "awdawd"
    This reverts commit 0e6a05fb944e37d25b0c390ca6b6efbb154ea32f.

--- Assets/Scripts/SpriteController.cs
+++ Assets/Scripts/SpriteController.cs
@@ -20,11 +20,7 @@ public class SpriteController : MonoBehaviour
         Vector3 cameraPosition = _mainCamera.transform.position;
         cameraPosition.y = transform.position.y;
 
-        // Calculate target rotation
-        Quaternion targetRotation = Quaternion.LookRotation(cameraPosition - transform.position);
-        targetRotation *= Quaternion.Euler(0f, 180f, 0f); // Flip the sprite
-
-        // Smoothly rotate towards the target rotation
-        transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, Time.deltaTime * 2f);
-    }
+        transform.LookAt(cameraPosition);
+        transform.Rotate(0f, 180f, 0f);
+    } 
 }
